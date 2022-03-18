9eaca37c0aab43ff8dcd02ada2b03f8ba012fe40
Marc Schmidt <marc.schmidt@serviceware.de>
2022-03-18 13:06:31 +0100

    Fix flaky UnitTests

--- src/Rocketcress.Core.Tests/LoggerTests.cs
+++ src/Rocketcress.Core.Tests/LoggerTests.cs
@@ -11,64 +11,73 @@ public class LoggerTests : TestClassBase
     [TestMethod]
     public void LogDebug_Success()
     {
+        var logStartTime = DateTime.Now;
         Logger.LogDebug("Test {0}", 4711);
-        AssertLogMessage("DBG - Test 4711", _traceListener.Text.TrimEnd());
+        AssertLogMessage(logStartTime, "DBG - Test 4711", _traceListener.Text.TrimEnd());
     }
 
     [TestMethod]
     public void LogInfo_Success()
     {
+        var logStartTime = DateTime.Now;
         Logger.LogInfo("Test {0}", 4711);
-        AssertLogMessage("INF - Test 4711", _traceListener.Text.TrimEnd());
+        AssertLogMessage(logStartTime, "INF - Test 4711", _traceListener.Text.TrimEnd());
     }
 
     [TestMethod]
     public void LogWarning_Success()
     {
+        var logStartTime = DateTime.Now;
         Logger.LogWarning("Test {0}", 4711);
-        AssertLogMessage("WRN - Test 4711", _traceListener.Text.TrimEnd());
+        AssertLogMessage(logStartTime, "WRN - Test 4711", _traceListener.Text.TrimEnd());
     }
 
     [TestMethod]
     public void LogError_Success()
     {
+        var logStartTime = DateTime.Now;
         Logger.LogError("Test {0}", 4711);
-        AssertLogMessage("ERR - Test 4711", _traceListener.Text.TrimEnd());
+        AssertLogMessage(logStartTime, "ERR - Test 4711", _traceListener.Text.TrimEnd());
     }
 
     [TestMethod]
     public void LogCritical_Success()
     {
+        var logStartTime = DateTime.Now;
         Logger.LogCritical("Test {0}", 4711);
-        AssertLogMessage("CRT - Test 4711", _traceListener.Text.TrimEnd());
+        AssertLogMessage(logStartTime, "CRT - Test 4711", _traceListener.Text.TrimEnd());
     }
 
     [TestMethod]
     public void Log_UnknownLogLevel_Success()
     {
+        var logStartTime = DateTime.Now;
         Logger.Log((LogLevel)4711, "Test {0}", 4711);
-        AssertLogMessage("___ - Test 4711", _traceListener.Text.TrimEnd());
+        AssertLogMessage(logStartTime, "___ - Test 4711", _traceListener.Text.TrimEnd());
     }
 
     [TestMethod]
     public void Log_NullParams_Success()
     {
+        var logStartTime = DateTime.Now;
         Logger.Log(LogLevel.Debug, "Test {0}", (object[])null);
-        AssertLogMessage("DBG - Test {0}", _traceListener.Text.TrimEnd());
+        AssertLogMessage(logStartTime, "DBG - Test {0}", _traceListener.Text.TrimEnd());
     }
 
     [TestMethod]
     public void Log_NoParams_Success()
     {
+        var logStartTime = DateTime.Now;
         Logger.Log(LogLevel.Debug, "Test {0}");
-        AssertLogMessage("DBG - Test {0}", _traceListener.Text.TrimEnd());
+        AssertLogMessage(logStartTime, "DBG - Test {0}", _traceListener.Text.TrimEnd());
     }
 
     [TestMethod]
     public void Log_ErrorWhileFormat_LogErrorMessage()
     {
+        var logStartTime = DateTime.Now;
         Logger.Log(LogLevel.Debug, "Test {0} {1}", 4711);
-        AssertLogMessage("ERR - Error while writing trace with message \"Test {0} {1}\" and parameters \"4711\": ", _traceListener.Text.TrimEnd(), Assert.StartsWith);
+        AssertLogMessage(logStartTime, "ERR - Error while writing trace with message \"Test {0} {1}\" and parameters \"4711\": ", _traceListener.Text.TrimEnd(), Assert.StartsWith);
     }
 
     protected override void OnInitializeTest()
@@ -85,13 +94,15 @@ public class LoggerTests : TestClassBase
         base.OnCleanupTest();
     }
 
-    private void AssertLogMessage(string expectedMessage, string actualLogMessage, Action<string, string> messageAssert = null)
+    private void AssertLogMessage(DateTime logStartTime, string expectedMessage, string actualLogMessage, Action<string, string> messageAssert = null)
     {
+        logStartTime = new DateTime(logStartTime.Year, logStartTime.Month, logStartTime.Day, logStartTime.Hour, logStartTime.Minute, logStartTime.Second);
+
         var splitMessage = actualLogMessage.Split(':');
         var timespan = DateTime.ParseExact(string.Join(":", splitMessage.Take(3)), "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
         var message = string.Join(":", splitMessage.Skip(3)).TrimStart();
 
-        Assert.IsBetween(DateTime.Now.AddSeconds(-1), DateTime.Now.AddSeconds(1), timespan);
+        Assert.IsBetween(logStartTime, logStartTime.AddSeconds(1), timespan, isMinInclusive: true, isMaxInclusive: true);
         (messageAssert ?? Assert.AreEqual)(expectedMessage, message);
     }
 
